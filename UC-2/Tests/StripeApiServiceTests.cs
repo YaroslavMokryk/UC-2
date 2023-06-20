6db4e8b1f6ffad214f61c468ab64770db494c1bf
Yaroslav Mokryk <yaroslav.mokryk@bankjoy.com>
2023-06-20 14:32:16 +0300

    UC #2 unit tests

---
+++ UC-2/Tests/StripeApiServiceTests.cs
@@ -0,0 +1,140 @@
+﻿using Moq;
+using Stripe;
+using System.Xml.Linq;
+using UC_2.Services;
+using Xunit;
+
+namespace UC_2.Tests
+{
+    public class StripeApiServiceTests
+    {
+        private readonly Mock<BalanceService> _stripeBalanceService;
+        private readonly Mock<BalanceTransactionService> _stripeBalanceTransactionService;
+
+        public StripeApiServiceTests()
+        {
+            _stripeBalanceService = new Mock<BalanceService>();
+            _stripeBalanceTransactionService = new Mock<BalanceTransactionService>();
+        }
+
+        [Fact]
+        public async Task GetStripeBalance_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balance = new Balance();
+            _stripeBalanceService.Setup(x => x.GetAsync(null, ct))
+                .ReturnsAsync(balance);
+            var service = new StripeApiService(_stripeBalanceService.Object, _stripeBalanceTransactionService.Object);
+
+            //Act
+            var result = await service.GetStripeBalance(ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(balance, result);
+        }
+
+        [Fact]
+        public async Task GetStripeBalance_StripeError_ThrowsException()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var ex = new StripeException
+            {
+                StripeError = new StripeError
+                {
+                    Message = "test"
+                }
+            };
+            _stripeBalanceService.Setup(x => x.GetAsync(null, ct))
+                .ThrowsAsync(ex);
+            var service = new StripeApiService(_stripeBalanceService.Object, _stripeBalanceTransactionService.Object);
+
+            //Act
+            Exception result = await Assert.ThrowsAsync<BadHttpRequestException>(() => service.GetStripeBalance(ct));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result);
+            Assert.NotNull(result.Message);
+            Assert.Equal(ex.StripeError.Message, result.Message);
+        }
+
+        [Fact]
+        public async Task GetStripeBalanceTransactions_NoPagination_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balanceTransactions = new StripeList<BalanceTransaction>()
+            {
+                Data = new List<BalanceTransaction>
+                {
+                    new BalanceTransaction { Id = "test" },
+                    new BalanceTransaction { Id = "test2" },
+                }
+            };
+            _stripeBalanceTransactionService.Setup(x => x.ListAsync(It.IsAny<BalanceTransactionListOptions>(), null, ct))
+                .ReturnsAsync(balanceTransactions);
+            var service = new StripeApiService(_stripeBalanceService.Object, _stripeBalanceTransactionService.Object);
+
+            //Act
+            var result = await service.GetStripeBalanceTransactions(null, string.Empty, ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(balanceTransactions, result);
+            Assert.Equal(balanceTransactions.Data, result.Data);
+        }
+
+        [Fact]
+        public async Task GetStripeBalanceTransactions_Pagination_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balanceTransactions = new StripeList<BalanceTransaction>()
+            {
+                Data = new List<BalanceTransaction>
+                {
+                    new BalanceTransaction { Id = "test2" },
+                }
+            };
+            _stripeBalanceTransactionService.Setup(x => x.ListAsync(It.IsAny<BalanceTransactionListOptions>(), null, ct))
+                .ReturnsAsync(balanceTransactions);
+            var service = new StripeApiService(_stripeBalanceService.Object, _stripeBalanceTransactionService.Object);
+
+            //Act
+            var result = await service.GetStripeBalanceTransactions(100, "test", ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(balanceTransactions, result);
+            Assert.Equal(balanceTransactions.Data, result.Data);
+        }
+
+        [Fact]
+        public async Task GetStripeBalanceTransactions_StripeError_ThrowsException()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var ex = new StripeException
+            {
+                StripeError = new StripeError
+                {
+                    Message = "test"
+                }
+            };
+            _stripeBalanceTransactionService.Setup(x => x.ListAsync(It.IsAny<BalanceTransactionListOptions>(), null, ct))
+                .ThrowsAsync(ex);
+            var service = new StripeApiService(_stripeBalanceService.Object, _stripeBalanceTransactionService.Object);
+
+            //Act
+            Exception result = await Assert.ThrowsAsync<BadHttpRequestException>(() => service.GetStripeBalanceTransactions(null, string.Empty, ct));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Message);
+            Assert.Equal(ex.StripeError.Message, result.Message);
+        }
+    }
+}
---
+++ UC-2/Tests/StripeControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using Moq;
+using Stripe;
+using UC_2.Controllers;
+using UC_2.Services;
+using Xunit;
+
+namespace UC_2.Tests
+{
+    public class StripeControllerTests
+    {
+        private readonly Mock<IStripeApiService> _stripeApiService;
+
+        public StripeControllerTests()
+        {
+            _stripeApiService = new Mock<IStripeApiService>();
+        }
+
+        [Fact]
+        public async Task GetStripeBalance_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balance = new Balance();
+            _stripeApiService.Setup(x => x.GetStripeBalance(ct))
+                .ReturnsAsync(balance);
+            var controller = new StripeController(_stripeApiService.Object);
+
+            //Act
+            var result = await controller.GetStripeBalance(ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.Equal(balance, result.Value);
+        }
+
+        [Fact]
+        public async Task GetStripeBalanceTransactions_NoPagination_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balanceTransactions = new StripeList<BalanceTransaction>()
+            {
+                Data = new List<BalanceTransaction>
+                {
+                    new BalanceTransaction { Id = "test" },
+                    new BalanceTransaction { Id = "test2" },
+                }
+            };
+            _stripeApiService.Setup(x => x.GetStripeBalanceTransactions(null, string.Empty, ct))
+                .ReturnsAsync(balanceTransactions);
+            var controller = new StripeController(_stripeApiService.Object);
+
+            //Act
+            var result = await controller.GetStripeBalanceTransactions(null, string.Empty, ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.Equal(balanceTransactions, result.Value);
+        }
+
+        [Fact]
+        public async Task GetStripeBalanceTransactions_Pagination_Success()
+        {
+            //Arrange
+            var ct = new CancellationToken();
+            var balanceTransactions = new StripeList<BalanceTransaction>()
+            {
+                Data = new List<BalanceTransaction>
+                {
+                    new BalanceTransaction { Id = "test2" },
+                }
+            };
+            _stripeApiService.Setup(x => x.GetStripeBalanceTransactions(100, "test", ct))
+                .ReturnsAsync(balanceTransactions);
+            var controller = new StripeController(_stripeApiService.Object);
+
+            //Act
+            var result = await controller.GetStripeBalanceTransactions(100, "test", ct);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.Equal(balanceTransactions, result.Value);
+            Assert.Equal(balanceTransactions.Data, result.Value.Data);
+        }
+    }
+}
